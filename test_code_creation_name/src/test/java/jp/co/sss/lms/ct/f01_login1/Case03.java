2ebfb16129ba6cee4a6b9f22e7d08f53f623e86b
ユーザー <@DESKTOP-F98KV7P>
2026-01-23 15:38:29 +0900

    add:Case03.javaへテストコードを記述、及びエビデンス3件を追加
    test1()：GoogleChromeを起動し、アドレスバーに下記URLを入力し、画面遷移する際、以下の期待値が得られるかテストコードを作成。
    取得したエビデンス「Case03_test01_01.png」はevidenceフォルダ内に格納。
    
    【URL】
    http://localhost:8080/lms/
    
    test2()：ログイン画面にて下記入力値を入力し、ログインボタンを押下した際、以下の期待値が得られるかについてテストを作成。
    取得したエビデンス「Case03_test02_01.png」「Case03_test02_02.png」はevidenceフォルダ内に格納。
    
    ■入力値
    ログインID：StudentAA01
    パスワード：Student8080
    
    ■期待値
    コース詳細画面へ画面遷移する

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
@@ -1,11 +1,15 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
@@ -35,14 +39,36 @@ public class Case03 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		goTo("http://localhost:8080/lms/");
+		// トップページに遷移した際のエビデンス01を取得
+		getEvidence(new Object() {}, "01");
+
+		String currentUrl = webDriver.getCurrentUrl();
+		assertEquals(currentUrl, "http://localhost:8080/lms/");
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		final WebElement loginId = webDriver.findElement(By.name("loginId"));
+		final WebElement password = webDriver.findElement(By.name("password"));
+		final WebElement loginBtn = webDriver.findElement(By.className("btn-primary"));
+
+		loginId.clear();
+		loginId.sendKeys("StudentAA01");
+		password.clear();
+		password.sendKeys("Student8080");
+
+		// 入力値を入力した際のエビデンス01を取得
+		getEvidence(new Object() {}, "01");
+
+		loginBtn.click();
+
+		// コース詳細画面へ画面遷移する際のエビデンス02を取得
+		getEvidence(new Object() {}, "02");
+
+		assertThat(webDriver.getTitle(), containsString("コース詳細"));
 	}
 
 }
