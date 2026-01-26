d90b514689a3632d30fc79453e45bd240f9b651f
ユーザー <@DESKTOP-F98KV7P>
2026-01-26 09:26:11 +0900

    add:Case05.javaへテストコードを記述、及びエビデンス9件を追加
    test1()：GoogleChromeを起動し、アドレスバーに下記URLを入力し、画面遷移する。
    画面遷移後に取得したエビデンス「Case05_test01_01.png」はevidenceフォルダ内に格納。
    
    【URL】
    http://localhost:8080/lms/
    
    test2()：ログイン画面にて下記入力値を入力し、ログインボタンを押下した際、以下の期待値が得られるかについてテストを作成。
    取得したエビデンス「Case05_test02_01.png」「Case04_test02_02.png」はevidenceフォルダ内に格納。
    
    ■入力値
    ログインID：StudentAA01
    パスワード：Student8080
    
    ■期待値
    コース詳細画面へ画面遷移する
    
    test3()：コース詳細画面にて、画面上部ヘッダー内「機能」プルダウンリストを展開し、「ヘルプ」を押下することで、以下の期待値が得られるかテストコードを作成。
    取得したエビデンス「Case05_test03_01.png」「Case05_test03_02.png」はevidenceフォルダ内に格納。
    
    ■期待値
    ヘルプ画面へ画面遷移する
    
    test4()：ヘルプ画面にて、「よくあるご質問はこちら」欄の「よくある質問」リンクを押下することで、以下の期待値が得られるかテストコードを作成。
    画面遷移後に取得したエビデンス「Case05_test04_01.png」はevidenceフォルダ内に格納。
    
    ■期待値
    新規タブを開き、よくある質問画面が表示される
    
    test5()：よくある質問画面にて、キーワード欄に下記入力値を入力し、キーワード欄下部の検索ボタンを押下することで、以下の期待値が得られるかテストコードを作成。
    取得したエビデンス「Case05_test05_01.png」「Case05_test05_02.png」はevidenceフォルダ内に格納。
    
    【入力値】
    キーワード：申し込み
    
    ■期待値
    入力値を含んだ質問のみが表示される
    
    test6()：よくある質問画面にて、キーワード欄下部のクリアボタンを押下することで、以下の期待値が得られるかテストコードを作成。
    取得したエビデンス「Case05_test06_01.png」はevidenceフォルダ内に格納。
    
    ■期待値
    キーワード欄に入力されていた入力値が消去される

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f02_faq/Case05.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f02_faq/Case05.java
@@ -1,6 +1,9 @@
 package jp.co.sss.lms.ct.f02_faq;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
@@ -11,6 +14,7 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 
 /**
@@ -38,41 +42,110 @@ public class Case05 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		goTo("http://localhost:8080/lms/");
+		// トップページに遷移した際のエビデンス01を取得
+		getEvidence(new Object() {}, "01");
+
+		String currentUrl = webDriver.getCurrentUrl();
+		assertEquals(currentUrl, "http://localhost:8080/lms/");
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		final WebElement loginId = webDriver.findElement(By.name("loginId"));
+		final WebElement password = webDriver.findElement(By.name("password"));
+		final WebElement loginBtn = webDriver.findElement(By.className("btn-primary"));
+
+		loginId.clear();
+		loginId.sendKeys("StudentAA01");
+		password.clear();
+		password.sendKeys("Student8080");
+
+		// 入力値を入力した際のエビデンス01を取得
+		getEvidence(new Object() {}, "01");
+
+		loginBtn.click();
+
+		// コース詳細画面へ画面遷移する際のエビデンス02を取得
+		getEvidence(new Object() {}, "02");
+
+		assertThat(webDriver.getTitle(), containsString("コース詳細"));
 	}
 	
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 上部メニューの「ヘルプ」リンクからヘルプ画面に遷移")
 	void test03() {
-		// TODO ここに追加
+		final WebElement headerFunction = webDriver.findElement(By.className("dropdown"));
+		
+		headerFunction.click();
+		
+		final WebElement help = webDriver.findElement(By.linkText("ヘルプ"));
+		
+		// ヘルプ画面へ画面遷移する前のエビデンス01を取得
+		getEvidence(new Object() {}, "01");
+		
+		help.click();
+		
+		// ヘルプ画面へ画面遷移した後のエビデンス02を取得
+		getEvidence(new Object() {}, "02");
+		
+		assertThat(webDriver.getTitle(), containsString("ヘルプ"));
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「よくある質問」リンクからよくある質問画面を別タブに開く")
 	void test04() {
-		// TODO ここに追加
+		final WebElement faq = webDriver.findElement(By.linkText("よくある質問"));
+		
+		faq.click();
+		
+		// webDriverにタブの切り替えを認識させるために、現在開いているウィンドウを「開いた順」にリスト要素へ追加
+		Object[] windowHandles = webDriver.getWindowHandles().toArray();
+		// 別タブで開かれた「よくある質問画面」へ操作を切り替える
+		webDriver.switchTo().window((String) windowHandles[1]);
+		
+		// よくある質問画面へ画面遷移した後のエビデンス01を取得
+		getEvidence(new Object() {}, "01");
+		
+		assertThat(webDriver.getTitle(), containsString("よくある質問"));
 	}
 	@Test
 	@Order(5)
 	@DisplayName("テスト05 キーワード検索で該当キーワードを含む検索結果だけ表示")
 	void test05() {
-		// TODO ここに追加
+		final WebElement keywordSearch = webDriver.findElement(By.className("form-control"));
+		final WebElement searchBtn = webDriver.findElement(By.cssSelector("input[value='検索']"));
+		
+		keywordSearch.sendKeys("申し込み");
+		
+		// 入力値を入力した際のエビデンス01を取得
+		getEvidence(new Object() {}, "01");
+		
+		searchBtn.click();
+		
+		// 検索結果を表示させた後、エビデンス02を取得
+		getEvidence(new Object() {}, "02");
+		
+		assertThat(webDriver.getPageSource(), containsString("研修の申し込みはどのようにすれば良いですか？"));
 	}
 	
 	@Test
 	@Order(6)
 	@DisplayName("テスト06 「クリア」ボタン押下で入力したキーワードを消去")
 	void test06() {
-		// TODO ここに追加
+		final WebElement clearBtn = webDriver.findElement(By.cssSelector("input[value='クリア']"));
+		final WebElement keywordSearch = webDriver.findElement(By.className("form-control"));
+		clearBtn.click();
+		
+		// クリアボタンをクリックした後、エビデンス01を取得
+		getEvidence(new Object() {}, "01");
+		
+		// 入力欄が空欄になっているか判定
+		assertEquals("", keywordSearch.getAttribute("value"));
 	}
 
 }
