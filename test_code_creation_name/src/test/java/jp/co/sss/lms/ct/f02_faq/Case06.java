208b00a746ad0640bae1b41ab6b82bebd07b79cc
ユーザー <@DESKTOP-F98KV7P>
2026-01-26 10:59:49 +0900

    add:Case06.javaへテストコードを記述、及びエビデンス8件を追加
    test1()：GoogleChromeを起動し、アドレスバーに下記URLを入力し、画面遷移する。
    画面遷移後に取得したエビデンス「Case06_test01_01.png」はevidenceフォルダ内に格納。
    
    【URL】
    http://localhost:8080/lms/
    
    test2()：ログイン画面にて下記入力値を入力し、ログインボタンを押下した際、以下の期待値が得られるかについてテストを作成。
    取得したエビデンス「Case06_test02_01.png」「Case06_test02_02.png」はevidenceフォルダ内に格納。
    
    ■入力値
    ログインID：StudentAA01
    パスワード：Student8080
    
    ■期待値
    コース詳細画面へ画面遷移する
    
    test3()：コース詳細画面にて、画面上部ヘッダー内「機能」プルダウンリストを展開し、「ヘルプ」を押下することで、以下の期待値が得られるかテストコードを作成。
    取得したエビデンス「Case06_test03_01.png」「Case06_test03_02.png」はevidenceフォルダ内に格納。
    
    ■期待値
    ヘルプ画面へ画面遷移する
    
    test4()：ヘルプ画面にて、「よくあるご質問はこちら」欄の「よくある質問」リンクを押下することで、以下の期待値が得られるかテストコードを作成。
    画面遷移後に取得したエビデンス「Case06_test04_01.png」はevidenceフォルダ内に格納。
    
    ■期待値
    新規タブを開き、よくある質問画面が表示される
    
    test5()：よくある質問画面にて、画面中央付近のカテゴリ検索より「【研修関係】」リンクを押下することで、以下の期待値が得られるかテストコードを作成。
    取得したエビデンス「Case06_test05_01.png」はevidenceフォルダ内に格納。
    
    ■期待値
    画面下部の検索結果に【研修関係】の検索結果が表示される
    
    test6()：「【研修関係】」リンク押下後のよくある質問画面にて、検索結果の質問「キャンセル料・途中退校について」をクリックすることで、以下の期待値が得られるかテストコードを作成。
    取得したエビデンス「Case06_test06_01.png」はevidenceフォルダ内に格納。
    
    ■期待値
    「Q.キャンセル料・途中退校について」の直下に、
    「A. 受講者の退職や解雇等、やむを得ない事情による途中終了に関してなど、事情をお伺いした上で、協議という形を取らせて頂きます。
    弊社営業担当までご相談下さい。」
     が表示される

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f02_faq/Case06.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f02_faq/Case06.java
@@ -1,18 +1,22 @@
 package jp.co.sss.lms.ct.f02_faq;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
 /**
- * 結合テスト よくある質問機能
- * ケース06
+ * 結合テスト よくある質問機能 ケース06
+ * 
  * @author holy
  */
 @TestMethodOrder(OrderAnnotation.class)
@@ -35,42 +39,104 @@ public class Case06 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		goTo("http://localhost:8080/lms/");
+		// トップページに遷移した際のエビデンス01を取得
+		getEvidence(new Object() {}, "01");
+
+		String currentUrl = webDriver.getCurrentUrl();
+		assertEquals(currentUrl, "http://localhost:8080/lms/");
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		final WebElement loginId = webDriver.findElement(By.name("loginId"));
+		final WebElement password = webDriver.findElement(By.name("password"));
+		final WebElement loginBtn = webDriver.findElement(By.className("btn-primary"));
+
+		loginId.clear();
+		loginId.sendKeys("StudentAA01");
+		password.clear();
+		password.sendKeys("Student8080");
+
+		// 入力値を入力した際のエビデンス01を取得
+		getEvidence(new Object() {}, "01");
+
+		loginBtn.click();
+
+		// コース詳細画面へ画面遷移する際のエビデンス02を取得
+		getEvidence(new Object() {}, "02");
+
+		assertThat(webDriver.getTitle(), containsString("コース詳細"));
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 上部メニューの「ヘルプ」リンクからヘルプ画面に遷移")
 	void test03() {
-		// TODO ここに追加
+		final WebElement headerFunction = webDriver.findElement(By.className("dropdown"));
+
+		headerFunction.click();
+
+		final WebElement help = webDriver.findElement(By.linkText("ヘルプ"));
+
+		// ヘルプ画面へ画面遷移する前のエビデンス01を取得
+		getEvidence(new Object() {}, "01");
+
+		help.click();
+
+		// ヘルプ画面へ画面遷移した後のエビデンス02を取得
+		getEvidence(new Object() {}, "02");
+
+		assertThat(webDriver.getTitle(), containsString("ヘルプ"));
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「よくある質問」リンクからよくある質問画面を別タブに開く")
 	void test04() {
-		// TODO ここに追加
+		final WebElement faq = webDriver.findElement(By.linkText("よくある質問"));
+
+		faq.click();
+
+		// webDriverにタブの切り替えを認識させるために、現在開いているウィンドウを「開いた順」にリスト要素へ追加
+		Object[] windowHandles = webDriver.getWindowHandles().toArray();
+		// 別タブで開かれた「よくある質問画面」へ操作を切り替える
+		webDriver.switchTo().window((String) windowHandles[1]);
+
+		// よくある質問画面へ画面遷移した後のエビデンス01を取得
+		getEvidence(new Object() {}, "01");
+
+		assertThat(webDriver.getTitle(), containsString("よくある質問"));
 	}
 
 	@Test
 	@Order(5)
 	@DisplayName("テスト05 カテゴリ検索で該当カテゴリの検索結果だけ表示")
 	void test05() {
-		// TODO ここに追加
+		final WebElement categorySearch = webDriver.findElement(By.linkText("【研修関係】"));
+		categorySearch.click();
+
+		// カテゴリ検索の結果を表示させた後、エビデンス01を取得
+		getEvidence(new Object() {}, "01");
+
+		assertThat(webDriver.getPageSource(), containsString("研修の申し込みはどのようにすれば良いですか？"));
 	}
 
 	@Test
 	@Order(6)
 	@DisplayName("テスト06 検索結果の質問をクリックしその回答を表示")
 	void test06() {
-		// TODO ここに追加
+		final WebElement question = webDriver.findElement(By.className("sorting_1"));
+		question.click();
+
+		// 検索結果の質問をクリックさせた後、エビデンス01を取得
+		getEvidence(new Object() {}, "01");
+
+		final WebElement answer = webDriver.findElement(By.className("fs18"));
+		assertEquals("A. 受講者の退職や解雇等、やむを得ない事情による途中終了に関してなど、事情をお伺いした上で、協議という形を取らせて頂きます。 弊社営業担当までご相談下さい。",
+				answer.getText());
 	}
 
 }
