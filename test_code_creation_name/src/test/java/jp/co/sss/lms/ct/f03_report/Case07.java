c55f155d1d451fcfe47c308c36e1c3ea6efe1177
ユーザー <@DESKTOP-F98KV7P>
2026-01-29 10:37:21 +0900

    add:Case07.javaへテストコードを記述、及びエビデンス7件を追加
    test1()：GoogleChromeを起動し、アドレスバーに下記URLを入力し、画面遷移する。
    画面遷移後に取得したエビデンス「Case17_test01_01.png」はevidenceフォルダ内に格納。
    
    【URL】
    http://localhost:8080/lms/
    
    test2()：ログイン画面にて下記入力値を入力し、ログインボタンを押下した際、以下の期待値が得られるかについてテストを作成。
    取得したエビデンス「Case07_test02_01.png」「Case07_test02_02.png」はevidenceフォルダ内に格納。
    
    ■入力値
    ログインID：StudentAA01
    パスワード：Student8080
    ※初回ログイン済のアカウントでログイン試行する
    
    ■期待値
    コース詳細画面へ画面遷移する
    
    test3()：コース詳細画面にて、日報未提出の研修日の「詳細」ボタンを押下することで、以下の期待値が得られるかテストコードを作成。
    取得したエビデンス「Case07_test03_01.png」はevidenceフォルダ内に格納。
    
    ■期待値
    セクション詳細画面へ画面遷移する
    
    test04():セクション詳細画面にて「提出する」ボタンを押下することで、以下の期待値が得られるかテストコードを作成。
    取得したエビデンス「Case07_test04_01.png」はevidenceフォルダ内に格納。
    
    ■期待値
    レポート登録画面へ画面遷移する
    
    test05():レポート登録画面にて下記入力値を入力し、「提出する」ボタンを押下することで、以下の期待値が得られるかテストコードを作成。
    取得したエビデンス「Case07_test05_01.png」「Case07_test05_02.png」はevidenceフォルダ内に格納。
    
    ■入力値
    本日の報告内容をお書きください。：今日はやりました。
    
    ■期待値
    セクション詳細画面へ画面遷移し、提出ボタンの名称が「提出済日報を確認する」ボタンへ変化している

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f03_report/Case07.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f03_report/Case07.java
@@ -1,11 +1,17 @@
 package jp.co.sss.lms.ct.f03_report;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import java.util.List;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
@@ -35,35 +41,101 @@ public class Case07 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		goTo("http://localhost:8080/lms/");
+		
+		// ウィンドウサイズ最大化処理
+		webDriver.manage().window().maximize();
+		
+		// トップページに遷移した際のエビデンス01を取得
+		getEvidence(new Object() {}, "01");
+
+		String currentUrl = webDriver.getCurrentUrl();
+		assertEquals(currentUrl, "http://localhost:8080/lms/");
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		final WebElement loginId = webDriver.findElement(By.name("loginId"));
+		final WebElement password = webDriver.findElement(By.name("password"));
+		final WebElement loginBtn = webDriver.findElement(By.className("btn-primary"));
+
+		loginId.clear();
+		loginId.sendKeys("StudentAA01");
+		password.clear();
+		password.sendKeys("Student8080");
+
+		// 入力値を入力した際のエビデンス01を取得
+		getEvidence(new Object() {}, "01");
+
+		loginBtn.click();
+
+		// コース詳細画面へ画面遷移する際のエビデンス02を取得
+		getEvidence(new Object() {}, "02");
+
+		assertThat(webDriver.getTitle(), containsString("コース詳細"));
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 未提出の研修日の「詳細」ボタンを押下しセクション詳細画面に遷移")
 	void test03() {
-		// TODO ここに追加
+		final List<WebElement> sectionDetailBtnList = webDriver.findElements(By.cssSelector("input[type='submit'][value='詳細']"));
+		sectionDetailBtnList.get(0).click();
+		
+		// 画面遷移完了まで待ち処理実施
+		pageLoadTimeout(5);
+		
+		// 画面遷移が確認できたらエビデンス01を取得
+		getEvidence(new Object() {}, "01");
+		
+		assertThat(webDriver.getTitle(), containsString("セクション詳細"));
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「提出する」ボタンを押下しレポート登録画面に遷移")
 	void test04() {
-		// TODO ここに追加
+		final WebElement reportRegistBtn = webDriver.findElement(By.cssSelector("input[type='submit'][value='日報【デモ】を提出する']"));
+		reportRegistBtn.click();
+		
+		// 画面遷移完了まで待ち処理実施
+		pageLoadTimeout(5);
+				
+		// 画面遷移が確認できたらエビデンス01を取得
+		getEvidence(new Object() {}, "01");
+				
+		assertThat(webDriver.getTitle(), containsString("レポート登録"));
+		
 	}
 
 	@Test
 	@Order(5)
 	@DisplayName("テスト05 報告内容を入力して「提出する」ボタンを押下し確認ボタン名が更新される")
 	void test05() {
-		// TODO ここに追加
+		final WebElement dailyReport = webDriver.findElement(By.id("content_0"));
+		final WebElement dailyReportRegistBtn = webDriver.findElement(By.className("btn-primary"));
+		
+		dailyReport.clear();
+		dailyReport.sendKeys("今日はやりました。");
+		
+		// 日報内容を記入した際にエビデンス01を取得
+		getEvidence(new Object() {}, "01");
+		
+		dailyReportRegistBtn.click();
+		
+		// 画面遷移完了まで待ち処理実施
+		pageLoadTimeout(5);
+						
+		// 画面遷移が確認できたらエビデンス02を取得
+		getEvidence(new Object() {}, "02");
+						
+		assertThat(webDriver.getTitle(), containsString("セクション詳細"));
+		
+		final WebElement reportRegisteredBtn = webDriver.findElement(By.cssSelector("input[type='submit'][value='提出済み日報【デモ】を確認する']"));
+		assertTrue(reportRegisteredBtn.isDisplayed());
+		
 	}
 
 }
