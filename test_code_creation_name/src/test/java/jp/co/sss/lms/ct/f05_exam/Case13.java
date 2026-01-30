b59227b9e9350abee4d30fb676f9959dc18bacb6
ユーザー <@DESKTOP-F98KV7P>
2026-01-30 11:44:41 +0900

    add:Case13.javaへテストコードを記述、及びエビデンス21件を追加
    test1()：GoogleChromeを起動し、アドレスバーに下記URLを入力し、画面遷移する。
    画面遷移後に取得したエビデンス「Case08_test01_01.png」はevidenceフォルダ内に格納。
    
    【URL】
    http://localhost:8080/lms/
    
    test2()：ログイン画面にて下記入力値を入力し、ログインボタンを押下した際、以下の期待値が得られるかについてテストを作成。
    取得したエビデンス「Case09_test02_01.png」「Case09_test02_02.png」はevidenceフォルダ内に格納。
    
    ■入力値
    ログインID：StudentAA01
    パスワード：Student8080
    ※初回ログイン済のアカウントでログイン試行する
    
    ■期待値
    コース詳細画面へ画面遷移する
    
    test3()：コース詳細画面にて、試験有」の研修日の「詳細」ボタンを押下することで、以下の期待値が得られるかテストコードを作成。
    
    取得したエビデンス「Case09_test03_01.png」はevidenceフォルダ内に格納。
    
    ■期待値
    セクション詳細画面へ画面遷移する
    
    test04()：セクション詳細画面にて、「本日の試験」エリアの「詳細」ボタンを押下することで、以下の期待値が得られるかテストコードを作成
    取得したエビデンス「Case09_test04_01.png」はevidenceフォルダ内に格納。
    
    ■期待値
    該当の試験開始画面へ画面遷移する
    
    test05():試験開始画にて「試験を開始する」ボタンを押下することで、以下の期待値が得られるかテストコードを作成。
    取得したエビデンス「Case09_test05_01.png」「Case08_test05_02.png」はevidenceフォルダ内に格納。
    
    ■期待値
    試験問題画面へ画面遷移する
    
    test06()：試験問題画面録画面にて全問未回答の状態で「確認画面へ進む」ボタンを押下することで、以下の期待値が得られるかテストコードを作成。
    取得したエビデンス「Case09_test06_01.png」～「Case09_test06_13.png」（※計13件）はevidenceフォルダ内に格納。
    
    ■期待値
    試験回答確認画面へ画面遷移する
    
    test07()：試験回答確認画面にて「回答を送信する」ボタンを押下することで、以下の期待値が得られるかテストコードを作成。
    取得したエビデンス「Case09_test07_01.png」「Case09_test07_02.png」はevidenceフォルダ内に格納。
    
    ■期待値
    試験結果画面へ画面遷移する
    試験結果画面にて、ITリテラシー①の右側に「あなたのスコア：0.0点」と表示される
    
    test08()：試験結果画面にて「戻る」ボタンを押下し試験開始画面に遷移することで、以下の期待値が得られるかテストコードを作成。
    取得したエビデンス「Case09_test08_01.png」「Case09_test08_02.png」はevidenceフォルダ内に格納。
    
    ■期待値
    当該試験の結果が反映され、スコア欄に0.0点が表示される

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f05_exam/Case13.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f05_exam/Case13.java
@@ -1,20 +1,26 @@
 package jp.co.sss.lms.ct.f05_exam;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
 
 import java.util.Date;
+import java.util.List;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
 /**
- * 結合テスト 試験実施機能
- * ケース13
+ * 結合テスト 試験実施機能 ケース13
+ * 
  * @author holy
  */
 @TestMethodOrder(OrderAnnotation.class)
@@ -40,56 +46,187 @@ public class Case13 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		goTo("http://localhost:8080/lms/");
+
+		// ウィンドウサイズ最大化処理
+		webDriver.manage().window().maximize();
+
+		// トップページに遷移した際のエビデンス01を取得
+		getEvidence(new Object() {}, "01");
+
+		String currentUrl = webDriver.getCurrentUrl();
+		assertEquals(currentUrl, "http://localhost:8080/lms/");
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		final WebElement loginId = webDriver.findElement(By.name("loginId"));
+		final WebElement password = webDriver.findElement(By.name("password"));
+		final WebElement loginBtn = webDriver.findElement(By.className("btn-primary"));
+
+		loginId.clear();
+		loginId.sendKeys("StudentAA01");
+		password.clear();
+		password.sendKeys("Student8080");
+
+		// 入力値を入力した際のエビデンス01を取得
+		getEvidence(new Object() {}, "01");
+
+		loginBtn.click();
+
+		// コース詳細画面へ画面遷移する際のエビデンス02を取得
+		getEvidence(new Object() {}, "02");
+
+		assertThat(webDriver.getTitle(), containsString("コース詳細"));
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 「試験有」の研修日の「詳細」ボタンを押下しセクション詳細画面に遷移")
 	void test03() {
-		// TODO ここに追加
+		final List<WebElement> sectionDetailBtnList = webDriver
+				.findElements(By.cssSelector("input[type='submit'][value='詳細']"));
+
+		// 「試験有」の研修日の「詳細」ボタンをクリックする
+		sectionDetailBtnList.get(1).click();
+
+		// 画面遷移完了まで待ち処理実施
+		pageLoadTimeout(5);
+
+		// 画面遷移が確認できたらエビデンス01を取得
+		getEvidence(new Object() {}, "01");
+
+		assertThat(webDriver.getTitle(), containsString("セクション詳細"));
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「本日の試験」エリアの「詳細」ボタンを押下し試験開始画面に遷移")
 	void test04() {
-		// TODO ここに追加
+		final WebElement detailBtn = webDriver.findElement(By.cssSelector("input[type='submit'][value='詳細']"));
+
+		detailBtn.click();
+
+		// 画面遷移完了まで待ち処理実施
+		pageLoadTimeout(5);
+
+		// 画面遷移が確認できたらエビデンス01を取得
+		getEvidence(new Object() {}, "01");
+
+		assertThat(webDriver.getTitle(), containsString("試験【ITリテラシー①】"));
 	}
 
 	@Test
 	@Order(5)
 	@DisplayName("テスト05 「試験を開始する」ボタンを押下し試験問題画面に遷移")
 	void test05() {
-		// TODO ここに追加
+		final WebElement questionStartBtn = webDriver
+				.findElement(By.cssSelector("input[type=\"submit\"][value=\"試験を開始する\"]"));
+
+		questionStartBtn.click();
+
+		// 画面遷移完了まで待ち処理実施
+		pageLoadTimeout(5);
+
+		// 画面遷移が確認できたらエビデンス01を取得
+		getEvidence(new Object() {}, "01");
+
+		assertThat(webDriver.getTitle(), containsString("ITリテラシー①"));
 	}
 
 	@Test
 	@Order(6)
 	@DisplayName("テスト06 未回答の状態で「確認画面へ進む」ボタンを押下し試験回答確認画面に遷移")
 	void test06() {
-		// TODO ここに追加
+		final WebElement confirmationBtn = webDriver
+				.findElement(By.cssSelector("input[type=\"submit\"][value=\"確認画面へ進む\"]"));
+		final List<WebElement> panelHeadingList = webDriver.findElements(By.className("panel-heading"));
+
+		for (int i = 0; i < 12; i++) {
+			// エビデンス用のサフィックスを整形
+			String suffix = String.format("%02d", i + 1);
+
+			// 問題番号要素をリストから取得
+			WebElement panelHeading = panelHeadingList.get(i);
+
+			// 問題番号要素（初期値：[0]）ごとにスクロール処理を実行
+			((JavascriptExecutor) webDriver).executeScript("window.scrollTo(0, arguments[0].getBoundingClientRect().top + window.pageYOffset - 53);",panelHeading);
+			
+			// スクロール後、エビデンス「suffix」（初期値：01、12まで取得）を取得
+			getEvidence(new Object() {}, suffix);
+		}
+
+		confirmationBtn.click();
+
+		// 画面遷移完了まで待ち処理実施
+		pageLoadTimeout(5);
+
+		// 画面遷移が確認できたらエビデンス13を取得
+		getEvidence(new Object() {}, "13");
+
+		assertThat(webDriver.getTitle(), containsString("ITリテラシー①"));
+
 	}
 
 	@Test
 	@Order(7)
 	@DisplayName("テスト07 「回答を送信する」ボタンを押下し試験結果画面に遷移")
 	void test07() throws InterruptedException {
-		// TODO ここに追加
+		final WebElement sendBtn = webDriver.findElement(By.id("sendButton"));
+		
+		// 待ち処理によって時間を少し経過させ、回答時間に関するエラーを回避する
+		Thread.sleep(3000);
+		
+		scrollBy("2200");
+		
+		sendBtn.click();
+		
+		// アラートへ操作を切り替え、「OK」ボタンを押下
+		webDriver.switchTo().alert().accept();
+		
+		// 画面遷移完了まで待ち処理実施
+		pageLoadTimeout(5);
+
+		// 画面遷移が確認できたらエビデンス01を取得
+		getEvidence(new Object() {}, "01");
+		
+		final WebElement scoreResult = webDriver.findElement(By.tagName("h2"));
+
+		assertThat(webDriver.getTitle(), containsString("ITリテラシー①"));
+		assertThat(scoreResult.getText(), containsString("あなたのスコア：0.0点"));
 	}
 
 	@Test
 	@Order(8)
 	@DisplayName("テスト08 「戻る」ボタンを押下し試験開始画面に遷移後当該試験の結果が反映される")
 	void test08() {
-		// TODO ここに追加
+		final WebElement returnBtn = webDriver.findElement(By.cssSelector("input[type='submit'][value='戻る']"));
+		
+		scrollBy("5300");
+		
+		returnBtn.click();
+		
+		// 画面遷移完了まで待ち処理実施
+		pageLoadTimeout(5);
+		
+		// 過去の試験結果欄「詳細」ボタンをリストで取得
+		final List<WebElement> scoreHistoryDetailBtnList = webDriver.findElements(By.cssSelector("input[type=\"submit\"][value=\"詳細\"]"));
+		
+		// 過去の試験結果から最新の試験結果が表示されるまでスクロール
+		((JavascriptExecutor) webDriver).executeScript("window.scrollTo(0, arguments[0].getBoundingClientRect().top + window.pageYOffset - 53);",scoreHistoryDetailBtnList.getLast());
+		
+		// 最新の試験結果が確認できたらエビデンス01を取得
+		getEvidence(new Object() {}, "01");
+		
+		// 直近の試験結果が表示されている要素について、xpathを成型
+		String latestHistoryScoreXpath = "//*[@id=\"main\"]/div/table[2]/tbody/tr[" + (scoreHistoryDetailBtnList.size() + 1) +"]/td[2]";
+		WebElement latestHistoryScore = webDriver.findElement(By.xpath(latestHistoryScoreXpath));
+		
+		assertThat(webDriver.getTitle(), containsString("試験【ITリテラシー①】"));
+		assertThat(latestHistoryScore.getText(), containsString("0.0点"));
+		
 	}
 
 }
